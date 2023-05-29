63b7e82fc7c1cd5f1cd318afd7042f7cc42a0f90
Stefano Casasola <ste.casasola@gmail.com>
2023-05-29 11:37:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// UN FILE JS ESTERNO!
+
+// Il mestiere del developer si incentra sulla RISOLUZIONE di problemi. Nella creazione di un ALGORITMO!
+// Un algoritmo è una serie di passaggi, numerati, che porta alla soluzione di un problema.
+// Un algoritmo, se il problema è sempre lo stesso, vi porterà alla stessa soluzione.
+
+// al fine di scrivere un algoritmo, sarà quasi sempre fondamentale creare delle VARIABILI.
+// una variabile è un'allocazione di memoria dedicata alla memorizzazione di un valore.
+// possiamo immaginarla come una scatola che contiene qualcosa, o una bacinella con un ingrdiente nella realizz. di una ricetta.
+
+// VARIABILI
+// le variabili di dichiarano con la parola chiave (keyword) --> let
+let dog // creazione della variabile dog
+// ho creato una variabile chiamata "dog". Al momento questa variabile è VUOTA (è stata dichiarata, ma non ha nessun valore)
+dog = 'Fido' // assegnazione del valore "Fido" alla variabile che si chiama dog
+
+let cat = 'Tom' // ho creato la variabile cat e le assegnato un valore di "Tom" in un'unica riga
+
+// TIPI DI VALORI POSSIBILI PER UNA VARIABILE:
+// stringhe es. 'Mario', "Rossi", `Stefano`
+// numeri es. 1, 0, 150000, 25.2
+// booleani es. true, false
+// undefined
+// null
+
+// le variabili vengono nominate secondo la cosiddetta sintassi "camelCase"
+let favouriteNumber = 7 // senza delimitatori come apici, virgolette etc.!
+
+let canYouDrive = true // senza delimitatori come apici, virgolette etc.!
+
+// le variabili sono contenitori DINAMICI, è possibile cambiarne il valore successivamente!
+favouriteNumber = 10 // ho cambiato il valore di favouriteNumber
+canYouDrive = false
+
+// NON abituatevi a CAMBIARE il TIPO di una variabile
+// favouriteNumber = 'Stefano' // PESSIMA ABITUDINE!!! in altri linguaggi questa cosa non si può fare...
+
+// una variabile SI DICHIARA UNA VOLTA SOLA!
+// ma una variabile può venire assegnata/riassegnata n volte!
